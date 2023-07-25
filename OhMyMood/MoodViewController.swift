eb953a3dd2151d91c35e0a1e2a33ac234ab279d9
walkerhilla <huny98.dev@gmail.com>
2023-07-26 06:16:56 +0900

    UX 개선 - 터치 애니메이션 적용

--- OhMyMood/MoodViewController.swift
+++ OhMyMood/MoodViewController.swift
@@ -12,6 +12,8 @@ class MoodViewController: UIViewController {
   // MARK: - Properties
   
   @IBOutlet var moods: [UIView]!
+  @IBOutlet var emojis: [UIImageView]!
+  
   
   // MARK: - Lifecycle Methods
   
@@ -36,7 +38,18 @@ class MoodViewController: UIViewController {
   @objc func moodTapped(_ sender: UITapGestureRecognizer) {
     if let tappedMood = sender.view {
       let moodTag = tappedMood.tag
-      print(moodTag)
+      
+      MoodScoreManager.shared.increaseScore(moodTag)
+      
+      let emoji = emojis[moodTag]
+      // 애니메이션 효과를 위해 원래의 위치에서 약간 위로 올리고 다시 내려오는 애니메이션을 추가합니다.
+      UIView.animate(withDuration: 0.2, animations: {
+        emoji.transform = CGAffineTransform(translationX: 0, y: -10)
+      }) { (_) in
+        UIView.animate(withDuration: 0.2) {
+          emoji.transform = .identity // 원래 위치로 돌아오는 애니메이션
+        }
+      }
     }
   }
   
