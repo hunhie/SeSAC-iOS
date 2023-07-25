f7f5de2d804aa99e152d96be047dfc0e30c5eba3
walkerhilla <huny98.dev@gmail.com>
2023-07-26 06:15:01 +0900

    Fix MoodScoreManager.swift

--- OhMyMood/MoodScoreManager.swift
+++ OhMyMood/MoodScoreManager.swift
@@ -7,7 +7,10 @@
 
 import Foundation
 
-enum Mood {
+// 과제를 위해서 열거형과 딕셔너리를 활용해보았으나 합리적인 적용은 아닌 듯..
+
+// Mood 상태를 구분하는 열거형
+enum Mood: Int {
   case happy
   case smile
   case soso
@@ -15,18 +18,19 @@ enum Mood {
   case bad
 }
 
+// Mood 점수 매니저(싱글톤)
 class MoodScoreManager {
   static let shared = MoodScoreManager()
   
   private init() {}
   
-  var moodScore: [Mood: Int] = [.happy: 0, .smile: 0, .soso: 0, .sad: 0, .bad: 0]
+  var moodScoreDic: [Mood: Int] = [.happy: 0, .smile: 0, .soso: 0, .sad: 0, .bad: 0]
   
-  func increaseScore(_ mood: Mood) {
-    self.moodScore.updateValue(moodScore[mood]! + 1, forKey: mood)
+  func increaseScore(_ mood: Int) {
+    self.moodScoreDic.updateValue(moodScoreDic[Mood(rawValue: mood)!]! + 1, forKey: Mood(rawValue: mood)!)
   }
   
-  func getScore(_ mood: Mood) -> Int {
-    return self.moodScore[mood]!
+  func getScore(_ mood: Int) -> Int {
+    return self.moodScoreDic[Mood(rawValue: mood)!]!
   }
 }
