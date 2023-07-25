4e8e7a0661b8aebac9d4dae740a843670c8767df
walkerhilla <huny98.dev@gmail.com>
2023-07-26 05:26:58 +0900

    MoodScoreViewController 생성

---
+++ OhMyMood/MoodScoreViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MoodScoreViewController.swift
+//  OhMyMood
+//
+//  Created by walkerhilla on 2023/07/26.
+//
+
+import UIKit
+
+class MoodScoreViewController: UIViewController {
+  
+  // MARK: - Properties
+  
+  
+  // MARK: - Lifecycle Methods
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    configureUI()
+  }
+  
+  
+  // MARK: - UI Setup
+  
+  func configureUI() {
+    
+  }
+  
+  
+  // MARK: - Constraints
+  
+  // MARK: - IBActions
+  
+  // MARK: - Public Methods
+  
+  // MARK: - Private Methods
+
+}
