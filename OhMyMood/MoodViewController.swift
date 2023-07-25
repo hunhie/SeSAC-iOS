fe707126a29d1b5031e9891d9d0174e92c72808e
walkerhilla <huny98.dev@gmail.com>
2023-07-26 05:47:03 +0900

    Apply tapGestureRecognizer for Mood Views

--- OhMyMood/MoodViewController.swift
+++ OhMyMood/MoodViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class MoodViewController: UIViewController {
-
+  
+  // MARK: - Properties
+  
+  @IBOutlet var moods: [UIView]!
+  
+  // MARK: - Lifecycle Methods
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    configureUI()
+    setupMoods()
   }
-
-
+  
+  
+  // MARK: - UI Setup
+  
+  func configureUI() {
+  }
+  
+  
+  // MARK: - Constraints
+  
+  // MARK: - IBActions
+  
+  @objc func moodTapped(_ sender: UITapGestureRecognizer) {
+    if let tappedMood = sender.view {
+      let moodTag = tappedMood.tag
+      print(moodTag)
+    }
+  }
+  
+  // MARK: - Public Methods
+  
+  // MARK: - Private Methods
+  
+  private func setupMoods() {
+    moods.forEach { mood in
+      mood.isUserInteractionEnabled = true
+      mood.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(moodTapped(_:))))
+    }
+  }
 }
 
