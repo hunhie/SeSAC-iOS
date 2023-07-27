389f17811af62d458e69afe7989a5a3f4e182526
walkerhilla <huny98.dev@gmail.com>
2023-07-27 18:04:06 +0900

    Main UI

--- SeSACWeek3/ViewController.swift
+++ SeSACWeek3/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+  
+  let segues = ["toCase1", "toCase2", "toCase3"]
+  let sectionData = Section(title: "과제 목록", rows: ["case 1", "case 2", "case 3"])
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return sectionData.rows.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "caseCell")!
+    
+    cell.textLabel?.text = sectionData.rows[indexPath.row]
+    
+    return cell
+  }
 
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    performSegue(withIdentifier: segues[indexPath.row], sender: nil)
+  }
 }
 
