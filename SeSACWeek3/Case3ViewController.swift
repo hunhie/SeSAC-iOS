6240f324fec5d585fd119e67e325c4a58c69b0ed
walkerhilla <huny98.dev@gmail.com>
2023-07-27 19:24:47 +0900

    Case3 테이블 데이터 추가 및 새로고침

--- SeSACWeek3/Case3ViewController.swift
+++ SeSACWeek3/Case3ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class Case3ViewController: UIViewController {
   
+  // MARK: - Properties
+  
   @IBOutlet weak var tableView: UITableView!
   @IBOutlet weak var searchView: UIView!
   @IBOutlet weak var searchTextField: UITextField!
@@ -16,10 +18,15 @@ class Case3ViewController: UIViewController {
   
   var tasks: [String] = ["그립톡 구매하기", "맥북 거치대 구매하기", "애플 워치 구매하기", "아이폰 거치대 구매하기"]
   
+  // MARK: - Lifecycle Methods
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     configureUI()
   }
+  
+  // MARK: - UI Setup
   
   func configureUI() {
     setupSearchView()
@@ -35,40 +42,56 @@ class Case3ViewController: UIViewController {
     searchButton.clipsToBounds = true
   }
   
+  // MARK: - IBActions
+  
+  @IBAction func searchButtonTapped(_ sender: UIButton) {
+    guard let text = searchTextField.text,
+          text.count > 0 else { return }
+    tasks.append(text)
+    
+    tableView.reloadData()
+  }
+
+  // MARK: - Private Methods
+  
   func setupTableView() {
     tableView.delegate = self
     tableView.dataSource = self
-    
   }
 }
 
+
+// TableView Extension
 extension Case3ViewController: UITableViewDelegate, UITableViewDataSource {
   
+  // 섹션 개수
   func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
   
+  // 섹션 내 로우 개수
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    print("실행됨")
     return tasks.count
   }
   
+  // 로우 높이
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     return 60
   }
   
+  
+  // 로우 별 셀 설정
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    print(#function)
     let cell = tableView.dequeueReusableCell(withIdentifier: "case3Cell", for: indexPath) as! Case3Cell
     
     cell.layer.cornerRadius = 8
     cell.clipsToBounds = true
     cell.leftImageView.image = UIImage(systemName: "checkmark.square")
-    cell.leftImageView.contentMode = .scaleAspectFit
+    cell.leftImageView.tintColor = .black
     cell.rowLabel.text = tasks[indexPath.row]
     cell.rightImageView.image = UIImage(systemName: "star.fill")
+    cell.rightImageView.tintColor = .black
     
     return cell
   }
-
 }
