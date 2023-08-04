31c11883b6a6e1db8d878afba52ed4f3873c0857
walkerhilla <huny98.dev@gmail.com>
2023-08-04 19:24:02 +0900

    Add Constants file for strings and colors
    문자열과 색상 상수 파일을 추가함.

---
+++ Tamagotchi App/App/Constants/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  Tamagotchi App
+//
+//  Created by walkerhilla on 2023/08/04.
+//
+
+import UIKit
+
+/// Color Constants
+struct ColorConstant {
+  
+  static let primaryColor = UIColor.init(hexCode: "f5fcfc")
+  static let secondPrimaryColor = UIColor.init(hexCode: "eef9f9")
+  static let thirdPrimaryColor = UIColor.init(hexCode: "4d6a78")
+}
---
+++ Tamagotchi App/App/Constants/Strings.swift
@@ -0,0 +1,35 @@
+//
+//  strings.swift
+//  Tamagotchi App
+//
+//  Created by walkerhilla on 2023/08/04.
+//
+
+import UIKit
+
+/// String Constants
+struct StringConstant {
+  
+  // select Screen
+  static let tamagotchiSelectTitle = "다마고치 선택하기"
+  static let tamagotchiPreparing = "준비중이에요"
+  
+  // mainScreen
+  static let yourTamagotchi = "님의 다마고치"
+  static let eating = "밥먹기"
+  static let drinking = "물먹기"
+  static let riceGrain = "밥알"
+  static let waterDroplets = "물방울"
+  
+  // settings
+  static let setName = "내 이름 설정하기"
+  static let changeTamagotchi = "다마고치 변경하기"
+  static let reset = "데이터 초기화"
+  
+  // set name Screen
+  static let setNameTitle = "님 이름 정하기"
+  
+  // reset Screen
+  static let resetAlertTitle = "데이터 초기화"
+  static let resetAlertMessage = "정말 다시 처음부터 시작하실거에요?"
+}
