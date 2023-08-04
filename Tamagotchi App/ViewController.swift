0a5700aa2cb52d82958dd857b8a23c9f5dba6ecb
walkerhilla <huny98.dev@gmail.com>
2023-08-04 16:39:41 +0900

    Initial Commit

---
+++ Tamagotchi App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tamagotchi App
+//
+//  Created by walkerhilla on 2023/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
