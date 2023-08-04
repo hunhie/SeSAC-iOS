8f6ded756738795a4e0ab796349616b097aef8a8
walkerhilla <huny98.dev@gmail.com>
2023-08-04 21:07:09 +0900

    Complete Tamagotchi Selected Screen UI
    다마고치 선택 모달창 UI 완성.

--- Tamagotchi App/App/Controllers/TamagotchiSelectedModalViewController.swift
+++ Tamagotchi App/App/Controllers/TamagotchiSelectedModalViewController.swift
@@ -9,27 +9,106 @@ import UIKit
 
 final class TamagotchiSelectedModalViewController: UIViewController {
   
+  // MARK: - Properties
+  
   static let identifier = "TamagotchiSelectedModalViewController"
   
   @IBOutlet weak var modalBackView: UIView!
+  @IBOutlet weak var modalInnerView: UIView!
+  @IBOutlet weak var tamagotchiImageView: UIImageView!
+  @IBOutlet weak var tamagotchiNameView: UIView!
+  @IBOutlet weak var tamagotchiNameLabel: UILabel!
+  @IBOutlet weak var tamagotchiMessageLabel: UILabel!
+  @IBOutlet var dividers: [UIView]!
+  @IBOutlet weak var cancelButton: UIButton!
+  @IBOutlet weak var startButton: UIButton!
   
   var tamagotchi: Tamagotchi?
+  
+  // MARK: - Lifecycle Methods
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Do any additional setup after loading the view.
+    configureUI()
   }
   
   
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
+  // MARK: - UI Setup
   
+  func configureUI() {
+    
+    setupDivider()
+    setupModalBackView()
+    setupTamagotchiImageView()
+    setupTamagotchiNameView()
+    setupTamagotchiNameLabel()
+    setupTamagotchiMessageLabel()
+    setupButttons()
+  }
+  
+  func setupModalBackView() {
+    
+    modalInnerView.backgroundColor = .clear
+    modalBackView.backgroundColor = ColorConstant.primaryColor
+    modalBackView.layer.cornerRadius = 8
+    modalBackView.clipsToBounds = true
+  }
+  
+  func setupDivider() {
+    
+    dividers.forEach{ divider in
+      divider.backgroundColor = ColorConstant.dividerColor
+    }
+  }
+  
+  func setupTamagotchiImageView() {
+    
+    guard let tamagotchi else { return }
+    tamagotchiImageView.image = UIImage(named: tamagotchi.imagePathToString)
+  }
+  
+  func setupTamagotchiNameView() {
+    
+    tamagotchiNameView.backgroundColor = ColorConstant.secondPrimaryColor
+    tamagotchiNameView.layer.borderColor = ColorConstant.thirdPrimaryColor.cgColor
+    tamagotchiNameView.layer.borderWidth = 0.7
+    tamagotchiNameView.layer.cornerRadius = 5
+    tamagotchiNameView.clipsToBounds = true
+  }
+  
+  func setupTamagotchiNameLabel() {
+    
+    guard let tamagotchi else { return }
+    tamagotchiNameLabel.text = tamagotchi.name
+    tamagotchiNameLabel.font = .boldSystemFont(ofSize: 13)
+    tamagotchiNameLabel.textColor = ColorConstant.thirdPrimaryColor
+  }
+  
+  func setupTamagotchiMessageLabel() {
+    
+    guard let tamagotchi else { return }
+    tamagotchiMessageLabel.text = tamagotchi.profileMessage
+    tamagotchiMessageLabel.font = .systemFont(ofSize: 13)
+    tamagotchiMessageLabel.textColor = ColorConstant.thirdPrimaryColor
+    tamagotchiMessageLabel.textAlignment = .center
+    tamagotchiMessageLabel.numberOfLines = 0
+  }
+  
+  func setupButttons() {
+    
+    startButton.titleLabel?.text = "시작하기"
+    cancelButton.titleLabel?.text = "취소"
+    cancelButton.titleLabel?.font = .systemFont(ofSize: 14)
+    cancelButton.titleLabel?.textColor = ColorConstant.thirdPrimaryColor
+    startButton.titleLabel?.font = .systemFont(ofSize: 14)
+    startButton.titleLabel?.textColor = ColorConstant.thirdPrimaryColor
+    startButton.titleLabel?.text = "시작하기"
+  }
+  
+  
+  // MARK: - IBActions
+  @IBAction func handleTapGesture(_ sender: UITapGestureRecognizer) {
+    dismiss(animated: true)
+  }
 }
--- Tamagotchi App/App/Models/Tamagotchi.swift
+++ Tamagotchi App/App/Models/Tamagotchi.swift
@@ -22,7 +22,7 @@ struct Tamagotchi {
   let imagePath: tamagotchiImagePath
   
   var imagePathToString: String {
-    imagePath.rawValue+"\(level)"
+    imagePath.rawValue+"\(level < 10 ? level : 9)"
   }
 }
 
@@ -34,19 +34,17 @@ final class TamagotchiInfo {
     Tamagotchi(name: "따끔따끔 다마고치", profileMessage: """
                저는 따끔따끔 다마고치입니당~!
                절 만지면 손에 구멍이 뻥뻥 뚫릴지도 몰라요.
-               하루종일 뜨거운 햇빛을 받아서 너무 더워요.
+               하루종일 뜨거운 햇빛을 받아서 너무 덥네요!! 이게 다 방실방실 다마고치 때문이에요.
                얼른 해가 졌으면 좋겠다..
                """, level: 1, riceGrain: 0, waterDroplets: 0, imagePath: .tingling),
     Tamagotchi(name: "방실방실 다마고치", profileMessage: """
-               저는 방실방실 다마고치에요!
-               하늘을 밝게 빛나게 해주는 역할을 맡고 있어요.
-               따끔따금 다마고치가 저를 싫어하는 것 같더라구요..
-               가서 제 마음 좀 전해주실래요?
+               저는 방실방실 다마고치에요! 하늘을 밝게 빛나게 하는 역할을 맡고 있어요. 그런데, 따끔따끔 다마고치가 저를 싫어하는 것 같더라구요..
+               가서 제 마음을 전해주실래요?
                """, level: 1, riceGrain: 0, waterDroplets: 0, imagePath: .smile),
     Tamagotchi(name: "반짝반짝 다마고치", profileMessage: """
                밤 하늘의 길잡이 역할을 맡고 있는 반짝반짝 다마고치에요.
                불가사리 아니에요...
-               이래봬도 어엿한 별이랍니다.
+               이래봬도 어엿한 별이라구요!
                """, level: 1, riceGrain: 0, waterDroplets: 0, imagePath: .sparkle),
   ]
 }
