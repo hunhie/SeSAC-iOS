6d952f1ac29096395c1c3556136600c9edb7aae6
walkerhilla <huny98.dev@gmail.com>
2023-08-04 21:14:29 +0900

    Replace string literals with constants
    문자열 리터럴을 상수로 대체하였음.

--- Tamagotchi App/SceneDelegate.swift
+++ Tamagotchi App/SceneDelegate.swift
@@ -21,16 +21,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     window = UIWindow(windowScene: windowScene)
     
     // 앱 최초 실행 분기 로직
+    UserDefaults.standard.setValue(false, forKey: "isLaunched")
     let isLaunched = UserDefaults.standard.bool(forKey: "isLaunched")
     let sb = UIStoryboard(name: "Main", bundle: nil)
 
     if isLaunched {
-      let vc = sb.instantiateViewController(withIdentifier: "TamagotchiMainViewController") as! TamagotchiMainViewController
+      let vc = sb.instantiateViewController(withIdentifier: TamagotchiMainViewController.identifier) as! TamagotchiMainViewController
       let nav = UINavigationController(rootViewController: vc)
       window?.rootViewController = nav
     } else {
       UserDefaults.standard.setValue(true, forKey: "isLaunched")
-      let vc = sb.instantiateViewController(withIdentifier: "TamagotchiSelectViewController") as! TamagotchiSelectViewController
+      let vc = sb.instantiateViewController(withIdentifier: TamagotchiSelectViewController.identifier) as! TamagotchiSelectViewController
       window?.rootViewController = vc
     }
   
