c11f8b7cfdec0472887c80f430431a399ab7f048
walkerhilla <huny98.dev@gmail.com>
2023-08-05 15:24:37 +0900

    Update Tamagotchi model with modified properties 다마고치 모델 구조체의 프로퍼티를 수정하여 데이터를 업데이트 하였음.
    레벨, 물방울, 밥알을 저장하는 인스턴스 프로퍼티들을 타입 프로퍼티로 변경하여 모든 다마고치가 동일한 정보를 공유하도록 하였음.

--- Tamagotchi App/App/Models/Tamagotchi.swift
+++ Tamagotchi App/App/Models/Tamagotchi.swift
@@ -14,15 +14,23 @@ enum tamagotchiImagePath: String, Codable {
 }
 
 struct Tamagotchi: Codable {
+  static var level: Int {
+    let quotient = (riceGrain/5 + waterDroplets/2) / 10
+    return quotient > 0 ? quotient : 1
+  }
+  static var riceGrain: Int = 0
+  static var waterDroplets: Int = 0
+  
   let name: String
   let profileMessage: String
-  var level: Int
-  var riceGrain: Int
-  var waterDroplets: Int
   let imagePath: tamagotchiImagePath
   
   var imagePathToString: String {
-    imagePath.rawValue+"\(level < 10 ? level : 9)"
+    imagePath.rawValue+"\(Self.level < 10 ? Self.level : 9)"
+  }
+  
+  var defaultImagePathToString: String {
+    imagePath.rawValue+"6"
   }
 }
 
@@ -33,18 +41,17 @@ final class TamagotchiInfo {
   var tamagotchis: [Tamagotchi] = [
     Tamagotchi(name: "따끔따끔 다마고치", profileMessage: """
                저는 따끔따끔 다마고치입니당~!
-               절 만지면 손에 구멍이 뻥뻥 뚫릴지도 몰라요.
-               하루종일 뜨거운 햇빛을 받아서 너무 덥네요!! 이게 다 방실방실 다마고치 때문이에요.
+               뜨거운 햇살이 너무 더워요!! 이게 다 방실방실 다마고치 때문이에요.
                얼른 해가 졌으면 좋겠다..
-               """, level: 1, riceGrain: 0, waterDroplets: 0, imagePath: .tingling),
+               """, imagePath: .tingling),
     Tamagotchi(name: "방실방실 다마고치", profileMessage: """
                저는 방실방실 다마고치에요! 하늘을 밝게 빛나게 하는 역할을 맡고 있어요. 그런데, 따끔따끔 다마고치가 저를 싫어하는 것 같더라구요..
                가서 제 마음을 전해주실래요?
-               """, level: 1, riceGrain: 0, waterDroplets: 0, imagePath: .smile),
+               """, imagePath: .smile),
     Tamagotchi(name: "반짝반짝 다마고치", profileMessage: """
                밤 하늘의 길잡이 역할을 맡고 있는 반짝반짝 다마고치에요.
                불가사리 아니에요...
                이래봬도 어엿한 별이라구요!
-               """, level: 1, riceGrain: 0, waterDroplets: 0, imagePath: .sparkle),
+               """, imagePath: .sparkle),
   ]
 }
