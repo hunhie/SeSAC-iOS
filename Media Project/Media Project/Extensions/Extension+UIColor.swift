852d91c0dbaad33aff2c520704441135babba332
walkerhilla <huny98.dev@gmail.com>
2023-08-12 23:39:31 +0900

    [Update] code convention

--- Media Project/Media Project/Extensions/Extension+UIColor.swift
+++ Media Project/Media Project/Extensions/Extension+UIColor.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 extension UIColor {
-  
   convenience init(hexCode: String, alpha: CGFloat = 1.0) {
     var hexFormatted: String = hexCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
     
