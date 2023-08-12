534cb378f94e5c63081d055108cf7f288edb06a5
walkerhilla <huny98.dev@gmail.com>
2023-08-12 14:56:56 +0900

    Initial Project

---
+++ Media Project/Media Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Media Project
+//
+//  Created by walkerhilla on 2023/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
